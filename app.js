bd5038f940e44894a47263eb2f47bcaace80276e
Oleg Shanin <79807285+OlegShanin@users.noreply.github.com>
2025-10-29 19:56:59 +0100

    gallry

---
+++ app.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll('.slide')
+
+function slidesPlugin(activeSlide) {
+  slides[activeSlide].classList.add('active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+
+      slide.classList.add('active')
+    })
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
+}
+
+slidesPlugin(slides.length - 1)
